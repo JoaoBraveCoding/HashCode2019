2b7a7a0778ed94726f18b870408c912d32a5c471
Daniel-Fermoselle <daniel.fermoselle@gmail.com>
2019-02-28 19:25:33 +0000

    Compare Slides

--- hashcode/src/main.py
+++ hashcode/src/main.py
@@ -23,29 +23,37 @@ def make_ss(photos):
             del photos[rand2]
             current_photo = rand2
 
-def comparePhotos(t1,t2): #tags for p1 and p2
+def compare_slides(s1,s2): #tags for p1 and p2
+    t1 = get_all_tags(s1)
+    t2 = get_all_tags(s2)
     common = 0
     t1NotInT2 = 0
     t2NotInT1 = 0
     for el1 in t1:
-        tempResult = auxCompare(el1, t2)
+        tempResult = aux_compare(el1, t2)
         if tempResult == 1:
             common+=1
         else:
             t1NotInT2+=1
 
     for el2 in t2:
-        tempResult = auxCompare(el2, t1)
+        tempResult = aux_compare(el2, t1)
         if tempResult == 1:
             common+=1
         else:
             t2NotInT1+=1
 
-def auxCompare(el, t2):
+def aux_compare(el, t2):
     for el2 in t2:
         if(el == el2):
             return 1
     return 0
+
+def get_all_tags(s):
+    tags = ""
+    for tgs in s.tags:
+        tags += tgs
+    return tags
 
 def ss_out(slideshow):
     out = ""
